d785f7d45a0f7cee41f459e5d277543ae88fe0a5
drmortalwombat <90205530+drmortalwombat@users.noreply.github.com>
2023-04-16 22:59:11 +0200

    Add comments

--- 1320 ReflectingSprite/reflectingsprite.c
+++ 1320 ReflectingSprite/reflectingsprite.c
@@ -25,7 +25,10 @@ const char SpriteImage[64] = {
 	0b00000000, 0b11111111, 0b00000000, 	
 };
 
+// Screen memory
 char * const Screen = (char *)0x0400;
+
+// Unused system memory for sprite image
 char * const Sprite = (char *)0x0380;
 
 
@@ -36,17 +39,23 @@ struct RefSprite
 
 int main(void)
 {
+	// Copy sprite image into VIC available area
 	memcpy(Sprite, SpriteImage, 64);
 
+	// Initialize sprite data structures
 	spr_init(Screen);
 
 	for(char i=0; i<8; i++)
 	{
+		// Random position
 		sprites[i].sx = rand() % (320 - 24);
 		sprites[i].sy = rand() % (200 - 21);
+
+		// Random speed
 		sprites[i].vx = rand() % 5 - 2;
 		sprites[i].vy = rand() % 5 - 2;
 
+		// Initialize sprite
 		spr_set(i, true, 24 + sprites[i].sx, 50 + sprites[i].sy, (unsigned)Sprite / 64, i + 7, false, false, false);
 	}
 
@@ -54,22 +63,27 @@ int main(void)
 	{
 		for(char i=0; i<8; i++)
 		{
+			// Advance position			
 			int sx = sprites[i].sx + sprites[i].vx;
 			int sy = sprites[i].sy + sprites[i].vy;
 
+			// Check for reflection at left/right border
 			if (sx < 0 || sx > 320 - 24)
 				sprites[i].vx = -sprites[i].vx;
 			else
 				sprites[i].sx = sx;
 
+			// Check for reflection at top/bottom border
 			if (sy < 0 || sy > 200 - 21)
 				sprites[i].vy = -sprites[i].vy;
 			else
 				sprites[i].sy = sy;
 
+			// Update sprite position
 			spr_move(i, 24 + sprites[i].sx, 50 + sprites[i].sy);
 		}
 
+		// Wait one frame
 		vic_waitFrame();
 	}
 
