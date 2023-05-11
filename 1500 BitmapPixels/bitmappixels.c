94a40b289d017e005fb5605a0c5a2297765c90a8
drmortalwombat <90205530+drmortalwombat@users.noreply.github.com>
2023-05-11 08:01:15 +0200

    Documenting

--- 1500 BitmapPixels/bitmappixels.c
+++ 1500 BitmapPixels/bitmappixels.c
@@ -3,37 +3,65 @@
 #include <math.h>
 #include <string.h>
 
+// Place bitmap memory underneath kernal ROM
 char * const Hires	= (char *)0xe000;
+
+// Place cell color map underneath IO
 char * const Screen	= (char *)0xd000;
 
+// Set a single pixel
 void set(int x, int y)
 {
+	// Ensure that pixel is on screen
 	if (x >= 0 && x < 320 && y >= 0 && y < 200)
 	{
-		char	*	dp = Hires + 320 * (y >> 3) + (y & 7) + (x & ~7);
+		// Bitmap mode is similar to char mode, but each char cell
+		// is eight bytes and represents 8x8 pixels.
+
+		// So the first 8 horizontal pixels are in the first byte, the next
+		// are 8 bytes further
+		// The first 8 vertical pixels are in bytes 0 to 7, the next is
+		// in byte 320 (40 * 8)
+		
+		char	*	dp = Hires + 40 * 8 * (y >> 3) + (y & 7) + (x & ~7);
+
+		// Set the bit representing the pixel
 		*dp |= 0x80 >> (x & 7);
 	}
 }
 
 int main(void)
 {
+	// Install trampoline to keep system running with ROM paged out
 	mmap_trampoline();
+
+	// Page in RAM
 	mmap_set(MMAP_RAM);
+
+	// Set colors
 	memset(Screen, 0x01, 1000);
+
+	// Clear hires bitmap
 	memset(Hires, 0x00, 8000);
+
+	// Turn IO space back on
 	mmap_set(MMAP_NO_ROM);
 
+	// Set hires mode
 	vic_setmode(VICM_HIRES, Screen, Hires);
+	vic.color_border = VCOL_WHITE;
 
 	float	w = 0.0;
-
 	for(;;)
 	{
+		// Some fancy curves
 		int x = 160 + cos(w) * 80 + cos(w * 5) * 20 + cos(w * 13) * 10;
 		int y = 100 + sin(w * 2) * 80 + sin(w * 7) * 20 + sin(w * 11) * 10;
 
+		// Set the pixel
 		set(x, y);
 
+		// Advanve
 		w += 0.01;
 	}
 
--- 1520 BitmapColorImage/bitmapcolorimage.c
+++ 1520 BitmapColorImage/bitmapcolorimage.c
@@ -2,10 +2,14 @@
 #include <c64/memmap.h>
 #include <oscar.h>
 
+// Place bitmap memory underneath kernal ROM
 char * const Hires	= (char *)0xe000;
+
+// Place cell color map underneath IO
 char * const Screen	= (char *)0xd000;
 char * const Color	= (char *)0xd800;
 
+// Compressed bitmap image and color data
 static const char ImgHires[] = {
 	#embed 8000 0 lzo "../Resources/blumba.mcimg"
 };
@@ -20,16 +24,23 @@ static const char ImgColor[] = {
 
 int main(void)
 {
+	// Install trampoline to keep system running with ROM paged out
 	mmap_trampoline();
+
+	// Page in RAM
 	mmap_set(MMAP_RAM);
 
+	// Expand hires and first color map
 	oscar_expand_lzo(Hires, ImgHires);
 	oscar_expand_lzo(Screen, ImgScreen);
 
+	// Turn IO space back on
 	mmap_set(MMAP_ROM);
 
+	// Expand color attributes
 	oscar_expand_lzo(Color, ImgColor);
 
+	// Set hires multicolor mode
 	vic_setmode(VICM_HIRES_MC, Screen, Hires);
 	vic.color_border = VCOL_BLACK;
 	vic.color_back = VCOL_BLACK;
